515221067a1682a12e2482f8d264f62dcc1c9232
wu haifeng <wuhaifengdhu@163.com>
2025-01-04 23:22:19 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Devin's Personal Website</h1>
+        <p>
+          Hello, I am Devin Wu!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/DevinWu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome to my GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
